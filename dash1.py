f362c650133d8d22d063a23d8ba43cbc64c50354
Angeliki Beyko <Angeliki@Angelikis-MacBook-Air.local>
2015-09-26 18:23:46 -0500

    3rd commit

--- dash1.py
+++ dash1.py
@@ -41,11 +41,96 @@ class Home:
 
 	@cherrypy.expose
 	def style_1(self):
-		return "Work in Progress"
+		return """<html>
+			<head>
+				<STYLE>
+				<!--
+				A{text-decoration:none}
+				-->
+				</STYLE>
+				<link href="/static/css/style.css" rel="stylesheet">
+			</head>
+		<body>
+		<h3>
+			<table cellspacing="2" cellpadding="20">
+				<tbody>
+					<tr><
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Amazon-icon.png"></td>
+						<td><a href="http://www.amazon.com" style="color: #FFFFFF">Amazon.com</a></td>
+						<td>Status Icon</td>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Pinterest-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+					</tr>
+					<tr><
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Dropbox-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Reddit-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+					</tr>
+					<tr>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Evernote-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Skype-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+					</tr>
+					<tr>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Facebook-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/StumbleUpon-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+					</tr>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Flickr-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Tumblr-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+					<tr>
+					<tr></tr>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/GitHub-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Twitter-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+					<tr></tr>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Instagram-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Vine-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+					<tr></tr>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/LinkedIn-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+						<td width="64" align="center"><img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-circles/64/Youtube-icon.png"></td>
+						<td><a href="http://www.pinterest.com" style="color: #FFFFFF">Pinterest.com</a></td>
+						<td>Status Icon</td>
+				</tbody>
+			</table>
+		</h3>
+		</body>
+		</html>"""
 
 	@cherrypy.expose
 	def style_2(self):
-		return "Work in Progress"
+		hostname = "google.com" #example
+		response = os.system("ping -c 1 " + hostname)
+
+		#and then check the response...
+		if response == 0:
+				return hostname, ' is up!'
+		else:
+				return hostname, ' is down!'
+
 
 if __name__ == '__main__':
  conf = {
