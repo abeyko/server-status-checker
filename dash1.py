0f94384f33710c3610fff4e8b7b2f2460d17f666
Angeliki Beyko <Angeliki@Angelikis-MacBook-Air.local>
2015-09-26 14:30:09 -0500

    update cherrypy file to include html and css

--- dash1.py
+++ dash1.py
@@ -1,19 +1,43 @@
+import os, os.path
 import cherrypy
 
 class Home:
 
 	@cherrypy.expose
 	def index(self):
-		return "<a href='/links'>Web Service Monitoring Dashboard</a>"
+		return """<html>
+			<head>
+				<STYLE>
+				<!--
+				A{text-decoration:none}
+				-->
+				</STYLE>
+				<link href="/static/css/style.css" rel="stylesheet">
+			</head>
+		<body>
+			<h1><a href='/links' style="color: #FFFFFF">Web Service Monitoring <br>Dashboard</br></a></h1>
+			<h4><p>By: Angeliki Beyko</p></h4)
+		</body>
+	</html>"""
 
 	@cherrypy.expose
 	def links(self):
-		return """
-		<ul>
-			<li><a href='/style_1'>Style 1</a></li>
-			<li><a href='/style_2'>Style 2</a></li>
-		</ul>
-		"""
+		return """<html>
+			<head>
+				<STYLE>
+				<!--
+				A{text-decoration:none}
+				-->
+				</STYLE>
+				<link href="/static/css/style.css" rel="stylesheet">
+			</head>
+		<body>
+			<ul>
+				<h3><li><a href='/style_1' style="color: #FFFFFF">Style 1</a></li></h3>
+				<h3><li><a href='/style_2' style="color: #FFFFFF">Style 2</a></li></h3>
+			</ul>
+		</body>
+		</html>"""
 
 	@cherrypy.expose
 	def style_1(self):
@@ -23,5 +47,15 @@ class Home:
 	def style_2(self):
 		return "Work in Progress"
 
-home = Home()
-cherrypy.quickstart(home)
+if __name__ == '__main__':
+ conf = {
+     '/': {
+         'tools.sessions.on': True,
+         'tools.staticdir.root': os.path.abspath(os.getcwd())
+     },
+     '/static': {
+         'tools.staticdir.on': True,
+         'tools.staticdir.dir': './public'
+     }
+ }
+ cherrypy.quickstart(Home(), '/', conf)
