d9c22f507d2147a0f06bebb2193a7b5670aa66b0
Angeliki Beyko <Angeliki@Angelikis-MacBook-Air.local>
2015-10-11 08:58:16 -0500

    Commit No. 9 - Cherrypy links to html and css as separate files, but background image still needs work

---
+++ app.py
@@ -0,0 +1,20 @@
+import cherrypy
+import os
+
+class App():
+	@cherrypy.expose
+	def index(self):
+		return open("/Users/Angeliki/Desktop/icon_test/index.html",'r').read()
+
+if __name__ == '__main__':
+	conf = {
+	'/': {
+		 'tools.sessions.on': True,
+		 'tools.staticdir.root': os.path.abspath(os.getcwd())
+	 },
+	 '/static': {
+		 'tools.staticdir.on': True,
+		 'tools.staticdir.dir': './public'
+	 }
+	}
+cherrypy.quickstart(App(), '/', conf)
