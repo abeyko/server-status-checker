da674de1fca857404e183279f55bf0c0aa84fe1b
Angeliki Beyko <Angeliki@Angelikis-MacBook-Air.local>
2015-10-19 22:49:57 -0500

    Commit No. 22 - Ping timeout and HTTP toggle option, all sites are up

--- app.py
+++ app.py
@@ -1,50 +1,67 @@
-import atexit
-import threading
-import cherrypy 
+import cherrypy
 import os
-import MySQLdb 
+import MySQLdb
+import urllib
+import httplib
+import urllib2
+import requests
+
 
 class Root():
-	@cherrypy.expose
-	def index(self):
-		return open("/Users/Angeliki/Desktop/icon_test/public/index.html",'r').read()
-	
-	@cherrypy.expose
-	@cherrypy.tools.json_out()
-	def getData(self):
-		res = list()
-		global db
-		db=MySQLdb.connect(host="localhost", user="root", passwd="password", db="popular_sites")
-		c = db.cursor()
-		c.execute('SELECT site_url FROM popular_sites')
-		res = c.fetchall()
-		res = list(res)
-		c.close()
-		ser = []
-		for item in res:
 
-			response = os.system("ping -c 1 " + item[0])
-			
-			if response == 0:
-				ser.append("is up! :D")				
-			else:
-				ser.append("is down! :'(")				
+    @cherrypy.expose
+    def index(self):
+        return open("/Users/Angeliki/Desktop/icon_test/public/index.html",
+                    'r').read()
 
-		return {
-			'res' : ser
-		}
-	
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def getData(self):
+        res = list()
+        global db
+        db = MySQLdb.connect(
+            host="localhost", user="root",
+            passwd="password", db="popular_sites")
+        c = db.cursor()
+        c.execute('SELECT site_url FROM popular_sites')
+        res = c.fetchall()
+        res = list(res)
+        c.close()
+        ser = []
+        for item in res:
 
-if __name__ == '__main__': 
-	conf = {
-	'/': {
-		 'tools.sessions.on': True,
-		 'tools.staticdir.root': os.path.abspath(os.getcwd())
-	 },
-	 '/static': {
-		 'tools.staticdir.on': True,
-		 'tools.staticdir.dir': './public'
-	 }
-	}
-	
+            ping_response = os.system("ping -c 1 -W 5 " + item[0]) #should set a timeout for ping
+            
+            newitem = 'http://' + item[0] # method 3 works!
+            response = requests.get(newitem) # method 3 works!
+            response.history #(<Response [302]>, <Response[302]>, <Response [302]>) # method 3 works!
+            #for resp in response.history: # method 3 works!
+            	#print resp.status_code, resp.url # method 3 works!
+            #print response.status_code, response.url # method 3 works!
+            #print response.status_code # method 3 works!
+            http_response = response.status_code
+
+ 
+            if ping_response == 0 or http_response == 200:
+            	ser.append("is up! :D")
+            else:
+                ser.append("is down! :'(")
+
+        return {
+            'res': ser
+        }
+
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+
 cherrypy.quickstart(Root(), '/', conf)
