fb689f2b921a315a547aee66299d65654811374d
Angeliki Beyko <Angeliki@Angelikis-MacBook-Air.local>
2015-11-09 21:54:29 -0600

    Commit No. 35 - first httplib function integration into main app failed, next try plugins

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 import cherrypy
 import os
 import sqlite3
-#import requests
 import httplib
-import urlparse
-import urllib2
 import time
 
 
@@ -74,6 +71,46 @@ class App(object):
             c.execute("DELETE FROM my_sites WHERE Site_Url=?",
                       [the_url])
 
+    @cherrypy.expose
+    def resolve_httpredirect(url, depth=0):
+        headers = {"User-Agent": "firefox"}
+        body = ""
+        if depth > 10:
+            raise Exception("Redirected " + depth + "times, giving up.")
+        if url.startswith('http://'):
+            print "------------------START-------------------"
+            print "LEVEL: " + str(depth)
+            print "URL: " + url
+            url = url[7:]
+            url = str(url)
+            print "URL STRIPPED: " + url
+            conn = httplib.HTTPConnection(url)
+
+        if url.startswith('https://'):
+            print "LEVEL: " + str(depth)
+            print "URL: " + url
+            url = url[8:]
+            url = url[:-1]
+            url = str(url)
+            print "URL STRIPPED: " + url
+            conn = httplib.HTTPSConnection(url)
+
+        req = conn.request("HEAD", "/", body, headers)
+        res = conn.getresponse()
+        print "STATUS: " + str(res.status)
+        print "REASON: " + str(res.reason)
+
+        headers = dict(res.getheaders())
+        if 'location' in headers and headers['location'] != url:
+            print "NEW LOCATION: " + headers['location']
+            print "-----------------------------------------"
+            return resolve_httpredirect(headers['location'], depth + 1)
+        else:
+            final_status = str(res.status)
+            return final_status
+            print "-----------------FINISH-------------------"
+            print " "
+
     # Pings each url in site_url list from popular_sites table and returns
     # checkmark or cross wingding depending if the site is up or down
     # I want it to include both tables now...
@@ -102,123 +139,9 @@ class App(object):
             ping_response = os.system("ping -c 1 -W 5 " + ping_item)
             # try out pythonic version of ping, something compatible for
             # windows users
-
-            # METHOD 1 - gets status code, but needs to do redirect
-
-            http_item = item[0]
-            if http_item.startswith('http://'):
-                http_item = http_item[7:]
-                http_item = str(http_item)
-                print http_item
-                print "http_item " + http_item
-                print type(http_item)
-                response = httplib.HTTPConnection(http_item)  # new code works
-                # w/out the str(), it gives error saying cannot concoctenate
-                # str + instance objects
-                print "response is " + str(response)
-                response.request("HEAD", http_item)
-                #response = httplib.HTTPMessage(http_item)
-                # print "response with message is " + str(response)
-                new_response = response.getresponse()
-                print "new response is " + str(new_response)
-                status_code = new_response.status
-                print "status code is " + str(status_code)
-            """ 
-
-                ## METHOD 2 - attempt at redirecting
-            http_item = item[0]
-            depth = 0
-            if http_item.startswith('http://'):
-                http_item = http_item[7:]
-                print "http_item " + str(http_item)
-                o = urlparse.urlparse(http_item,allow_fragments = True)
-                print "o is " + str(o)
-                conn = httplib.HTTPConnection(o.netloc)
-                print "conn is " + str(conn)
-                path = o.path
-                print "path is " + str(path)
-
-                #query = len(o.query)
-                #print "o.query is of length " + str(query)
-                #print "o.query is " + str(o.query) + "before the if"
-                #if o.query:
-                 #   path += '?' + o.query
-                #print "o.query is " + str(o.query) + "after the if"
-                #print "o.query is of length " + str(query)
-
-                conn.request("HEAD", path)
-                new_res = conn.getresponse()
-                print "new_res is " + str(new_res)
-                headers = dict(new_res.getheaders())
-                print "headers is " + str(headers)
-                if headers.has_key('location') and headers['location'] != http_item:
-                    print "if statement" + str(resolve_httpredirect(headers['location'], depth+1))
-                    print "depth is " + str(depth)
-                else:
-                    print "else statement " + str(http_item)
-
-                """
-            # METHOD 3 urllib to get final url and then put through httplib
-            """
-            resp = urllib2.urlopen(item[0])
-            url_response = resp.geturl()
-            print "url_response is " + str(url_response)
-
-
-            
-            http_item = url_response
-            if http_item.startswith('http://'):
-                http_item = http_item[7:]
-                http_item = str(http_item)
-                print http_item
-                print "http_item " + http_item
-                print type(http_item)
-                response = httplib.HTTPConnection(http_item) # new code works
-                print "response is " + str(response) # w/out the str(), it gives error saying cannot concoctenate str + instance objects
-                response.request("HEAD", http_item)
-                #response = httplib.HTTPMessage(http_item)
-                #print "response with message is " + str(response)
-                #new_response = response.getresponse(response)
-                #print "new response is " + str(new_response)
-                status_code = new_response.status
-                print "status code is " + str(status_code)
-
-
-
-
-                
-                def resolve_httpredirect(url, depth=0):
-                    if depth > 10:
-                        raise Exception("Redirected " + depth + "times, giving up.")
-                        o = urlparse.urlparse(url,allow_fragments = True)
-                        conn = httplib.HTTPConnection(o.netloc)
-                        path = o.path
-                        if o.query:
-                            path += '?' + o.query
-                        conn.request("HEAD", path)
-                        new_res = conn.getresponse()
-                        headers = dict(new_res.getheaders())
-                        if headers.has_key('location') and headers['location'] != url:
-                            return resolve_httpredirect(headers['location'], depth+1)
-                        else:
-                            return url
-                            print "url is " + url
-                """
-
-            #status = response.getresponse()
-            # print "status is " + str(status)
-            ##new_response = response.request("HEAD", "/index.html")
-            # print "new response is " + str(new_response)
-            #response2 = httplib.HTTPConnection.request(response)
-
-            # message = httplib.HTTPResponse(response) # gives error: AttributeError: HTTPConnection instance has no attribute 'makefile'
-            # print "message is " + str(message)
-            # try to use httplib instead of requests
-            #http_response = response.status_code
-            # print response.status_code, response.url
-            # if ping_response == 0 or http_response == 200:
-            #http_status_check = httplib.responses[200]
-            if ping_response == 0:
+            test_url = item[0]
+            stat = resolve_httpredirect(testurl, 0)
+            if ping_response == 0 or stat == 200:
                 ser.append(u"\u2705")
             else:
                 ser.append(u"\u274e")
