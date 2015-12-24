08cf6d8bc06d8af591ebf493f5075698bcc16aa5
Angeliki Beyko <Angeliki@Angelikis-MacBook-Air.local>
2015-12-23 20:27:44 -0600

    Commit No. 54 - worked on DRY and object orientedness

--- app.py
+++ app.py
@@ -4,101 +4,71 @@ import sqlite3
 import httplib
 import time
 
+# icon | url | last checked | status icon | ping status | ping_latency | http_status code | weekly stats | delete | ping now 
 
-class App(object):
+class Database(object):
 
-    @cherrypy.expose
-    def index(self):
-        return open("./public/Site Monitoring Dashboard.html",
-                    'r').read()
-    @cherrypy.expose
-    def read_table(self, tables, table_columns):
-        """Return the site and icon urls
-        of the popular_sites and my_sites
-        tables in the sqlite3.db database."""
-        # remember to do something about timestamp, do it python side
-        # every time you ping, you add a new timestamp for that same site id
-        tables = ["popular_sites", "my_sites"]
-        table_columns = ["site_url", "icon_url"]
-        variables = {'popular_sites_urls', 'popular_sites_icons', 'my_sites_urls', 'my_sites_icons'}
-        print tables
-        print table_columns
-        connection = sqlite3.connect('sqlite3.db')
-        connection = connection.cursor()
+    url_list = []
+    def __init__(self):
+        self.connection = sqlite3.connect('sqlite3.db')
 
-        for table in tables:
-            for column in table_columns:
-                for item in dictionary:
-                    connection.execute("SELECT %s from %s" % column, table_type)
-                    item = connection.fetchall()
-                    print table
-                    print column
-                    print item
+    def read_table(self):
+        """Returns icon, url, last checked,
+        ping status, ping ping_latency,
+        and http status from database"""
+        #connection = sqlite3.connect('sqlite3.db')
+        connection = self.connection.cursor()
+        connection = connection.execute("SELECT * from sites")
+        table = connection.fetchall()
+
+        #global url_list
+        #url_list = []
+        icon_list = []
+        last_checked_list = []
+        ping_status_list = []
+        ping_latency_list = []
+        http_status_list = []
+
+        for item in table:
+            self.url_list.append(item[1])
+            icon_list.append(item[2])
+            ping_latency_list.append(item[3])
+            ping_status_list.append(item[4])
+            ping_latency_list.append(item[5])
+            http_status_list.append(item[6])
+
         connection.close()
+        print "in this method, list is " + str(self.url_list)
         return {
-            'popular_sites_urls': popular_sites_urls,
-            'popular_sites_icons': popular_sites_icons,
-            'my_sites_urls': my_sites_urls,
-            'my_sites_icons': my_sites_icons
+            'icon_list': icon_list,
+            'url_list': self.url_list,
+            'last_checked_list': last_checked_list,
+            'ping_status_list': ping_status_list,
+            'ping_latency_list': ping_latency_list,
+            'http_status_list': http_status_list
         }
 
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def update(self, table_type):
-        hi = "hi"
-
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def popular_sites_data(self):
-        """Return the site and icon urls
-        of the popular_sites database."""
-        connection = sqlite3.connect('sqlite3.db')
-        connection = connection.cursor()
-        connection.execute("SELECT site_url FROM popular_sites")
-        popular_sites_site_url_result = connection.fetchall()
-        connection.execute("SELECT icon_url FROM popular_sites")
-        popular_sites_icon_url_result = connection.fetchall()
-        connection.close()
-        return {
-            'site_url': popular_sites_site_url_result,
-            'icon_url': popular_sites_icon_url_result
-        }
-
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def my_sites_data(self):
-        """Return the site urls of the
-        popular_sites database."""
-        connection = sqlite3.connect('sqlite3.db')
-        connection = connection.cursor()
-        connection.execute("SELECT site_url FROM my_sites")
-        my_sites_site_url_result = connection.fetchall()
-        connection.close()
-        return {
-            'site_url': my_sites_site_url_result
-        }
-
-    @cherrypy.expose
-    def add_site(self, add_site):
-        """Add site to my_sites database.
+    def add_site(self, url):
+        """Add site to database.
 
         Keyword arguments:
         add_site -- the url of the site to be added
         """
-        with sqlite3.connect('sqlite3.db') as connection:
-            connection.execute("INSERT INTO my_sites(site_url) VALUES (?)",
-                               [add_site])
+        connection.execute("INSERT INTO sites(site_url) VALUES (?)",
+                           [url])
 
-    @cherrypy.expose
-    def delete_site(self, delete_site):
-        """Delete site from my_sites database.
+    def delete_site(self, url):
+        """Delete site from database.
 
         Keyword arguments:
         delete_site -- the url of the site to be deleted
         """
-        with sqlite3.connect('sqlite3.db') as connection:
-            connection.execute("DELETE FROM my_sites WHERE Site_Url=?",
-                               [delete_site])
+        connection.execute("DELETE FROM sites WHERE Site_Url=?",
+                               [url])
+
+global db = Database(object)
+
+class Site(object):
 
     @cherrypy.expose
     def httpredirect(self, url, depth=0):
@@ -154,22 +124,16 @@ class App(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def site_status(self):
-        """Return the status icon after checking each url
-        in the popular_sites and my_sites tables."""
-        connection = sqlite3.connect('sqlite3.db')
-        print "Connected to sqlite3 db via App class"
-        connection = connection.cursor()
-        connection.execute("SELECT site_url FROM popular_sites")
-        popular_sites_site_url_result = connection.fetchall()
-        connection.execute("SELECT site_url FROM my_sites")
-        my_sites_site_url_result = connection.fetchall()
-        connection.close()
+        """Return an up or down status icon after
+        checking each url in the database"""
+        time.sleep(10)
+        print "For checking purposes: in site_status, list is" + str(self.url_list)
         result_list = []
-        merged_db_tables = popular_sites_site_url_result + \
-            my_sites_site_url_result
-        for item in merged_db_tables:
-            ping_item = item[0]
-            http_item = item[0]
+        for url in self.url_list:
+            ping_item = url
+            http_item = url
+            print 'THIS IS URL'
+            print url
             if ping_item.startswith('http://'):
                 ping_item = ping_item[7:]
             ping_response = os.system("ping -c 1 -W 1 " + ping_item)
@@ -187,14 +151,6 @@ class App(object):
             'result': result_list
         }
 
-class Site(object):
-
-
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def __init__(self):
-        print "Site class is accessible"
-
     @cherrypy.expose
     def single_status_check(self):
         hi = "hi"
@@ -208,35 +164,27 @@ class Site(object):
         hi = "hi"
 
     @cherrypy.expose
-    def check_last_status(self):
-        hi = "hi"
-
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def add_site(self, add_site):
-        self.update(added_site)
-        connection.execute("INSERT INTO my_sites(site_url) VALUES (?)",
-                   [added_site])
-
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def delete_site(self, delete_site):
-        self.update(deleted_site)
-        connection.execute("DELETE FROM my_sites WHERE Site_Url=?",
-                   [deleted_site])
-
-    @cherrypy.expose
     def icon_upload(self):
         hi = "hi"
 
-
-class Database(object):
+    @cherrypy.expose
+    def add__site(self):
+        db.add_site(url)
 
     @cherrypy.expose
-    def __init__(self):
-        print "Database class is accessible"
-        self.connection = sqlite3.connect('sqlite3.db')
-        print "Connected to sqlite3 db via Database class"
+    def delete_site(self):
+        db.delete_site(url)
+
+    @cherrypy.expose
+    def read_table(self):
+        db.read_table()
+
+class App(object):
+
+    @cherrypy.expose
+    def index(self):
+        return open("./public/Site Monitoring Dashboard.html",
+                    'r').read()
 
 if __name__ == '__main__':
     conf = {
@@ -250,9 +198,182 @@ if __name__ == '__main__':
         }
     }
 
-cherrypy.app = App()
-cherrypy.app.site = Site()
-cherrypy.app.site.database = Database()
+#cherrypy.app = App()
+#cherrypy.app.site = Site()
+#cherrypy.app.site.database = Database()
 cherrypy.tree.mount(App(), '/', conf)
 cherrypy.engine.start()
 cherrypy.engine.block()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+# remember to do something about timestamp, do it python side
+# every time you ping, you add a new timestamp for that same site id
+# change sql to one table
+# for ping indiv. site - instance of site object
+# each site record is an object, can access
+# what if delete added site and it also erases one of the popular sites?
+# should popular sites now also have a delete function?
+# everything should have a ping now button?
+# then there would be no two tables?
+# it would be cool if the user could reorder the sites as they wish
+# add something where someone cannot add the same site if already listed in
+# pop sites, that's if we are sticking to same structure
+# pop sites
+# old
+# icon | url | last checked | status icon | weekly stats
+
+# new
+# icon | url | last checked | status icon | ping status | ping_latency | http_status code | weekly stats | delete | ping now 
+
+# my sites
+# old
+# url | last checked | status icon | weekly stats | delete
+#tables = ["popular_sites", "my_sites"]
+#table_columns = ["site_url", "icon_url"]
+# variables = {'popular_sites_urls', 'popular_sites_icons', 'my_sites_urls', 'my_sites_icons'}
+#variables = ['popular_sites_urls', 'popular_sites_icons', 'my_sites_urls', 'my_sites_icons']
+#print tables
+#print table_columns
+#print 'variables are '
+#print variables
+#connection = sqlite3.connect('sqlite3.db')
+
+
+for table in tables:
+    for column in table_columns:
+        for item in variables:
+            connection.execute("SELECT %s from %s" % (column, table))
+            item += connection.fetchall()
+            print table
+            print column
+            print item
+print 'new variables are '
+print variables
+
+return {
+    'popular_sites_urls': popular_sites_urls,
+    'popular_sites_icons': popular_sites_icons,
+    'my_sites_urls': my_sites_urls,
+    'my_sites_icons': my_sites_icons
+}
+#connection.close()
+
+        #connection = connection.cursor()
+        #connection = connection.execute("SELECT * from site")
+
+@cherrypy.expose
+@cherrypy.tools.json_out()
+def update_table(self):
+    hi = "hi"
+
+@cherrypy.expose
+@cherrypy.tools.json_out()
+def popular_sites_data(self):
+    Return the site and icon urls
+    of the popular_sites database.
+    connection = sqlite3.connect('sqlite3.db')
+    connection = connection.cursor()
+    connection.execute("SELECT site_url FROM popular_sites")
+    popular_sites_site_url_result = connection.fetchall()
+    connection.execute("SELECT icon_url FROM popular_sites")
+    popular_sites_icon_url_result = connection.fetchall()
+    connection.close()
+    return {
+        'site_url': popular_sites_site_url_result,
+        'icon_url': popular_sites_icon_url_result
+    }
+
+@cherrypy.expose
+@cherrypy.tools.json_out()
+def my_sites_data(self):
+    Return the site urls of the
+    popular_sites database.
+    connection = sqlite3.connect('sqlite3.db')
+    connection = connection.cursor()
+    connection.execute("SELECT site_url FROM my_sites")
+    my_sites_site_url_result = connection.fetchall()
+    connection.close()
+    return {
+        'site_url': my_sites_site_url_result
+    }
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def add_site(self, add_site):
+        self.update(added_site)
+        connection.execute("INSERT INTO my_sites(site_url) VALUES (?)",
+                   [added_site])
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def delete_site(self, delete_site):
+        self.update(deleted_site)
+        connection.execute("DELETE FROM my_sites WHERE Site_Url=?",
+                   [deleted_site])
+class Site(object):
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def __init__(self):
+        print "Site class is accessible"
+
+    @cherrypy.expose
+    def check_last_status(self):
+        hi = "hi"
+
+#class Database(object):
+
+connection = sqlite3.connect('sqlite3.db')
+        print "Connected to sqlite3 db via App class"
+        connection = connection.cursor()
+        connection.execute("SELECT site_url FROM popular_sites")
+        popular_sites_site_url_result = connection.fetchall()
+        connection.execute("SELECT site_url FROM my_sites")
+        my_sites_site_url_result = connection.fetchall()
+        connection.close()
+        result_list = []
+        merged_db_tables = popular_sites_site_url_result + \
+            my_sites_site_url_result
+
+
+"""
--- public/app.js
+++ public/app.js
@@ -1,3 +1,79 @@
+/** Gets site_url and icon_url return values from popular_sites_data Python function. */
+var get_popular_sites = $.ajax({
+    'url': '/read_table'
+});
+get_popular_sites.done(function(sites) {
+    var urls = sites.url_list;
+    console.log("urls is ");
+    console.log(urls);
+    var icons = sites.icon_list;
+    console.log("icons is ");
+    console.log(icons);
+    var site_urls = [];
+    var img_urls = [];
+    var table_string = "";
+    for (i = 0; i < urls.length; i++) {
+        site_urls.push(urls[i]);
+        img_urls.push(icons[i]);
+        console.log("SITE URLS ARE");
+        console.log("IMG URLS ARE");
+        console.log(site_urls);
+        console.log(img_urls);
+        table_string += "<tr><td align=\"center\" width=\"64\">" +
+            "<img src=" + img_urls[i] +
+            "></td><td style=\"color: #FFFFFF\">" + site_urls[i] +
+            "</td><td>" + "Last checked 2 seconds ago" + "</td><td id=" +
+            i.toString() + " style=\"font-size:200%\"></td><td>" +
+            "Ping Status: " + "</td><td>" +
+            "Ping Latency: " + "</td><td>" +
+            "HTTP Status: " + "</td><td>" +
+            "Weekly Stats" + "</td><td>" +
+            "<button type=\"button\" onclick=\"delete_the_site(&#34;" +
+            site_urls[i] + "&#34;, &#34;" + site_urls[i] +
+            "&#34;)\">Delete</button>" + "</td><td>" +
+            "Ping Now" + "</td></tr>";
+        console.log("IMG URLS [I] ARE");
+        console.log(img_urls[i]);
+    }
+    document.getElementById("sites").innerHTML = table_string;
+});
+/** Posts site to delete in delete_site Python function. */
+function delete_the_site(site, site_url) {
+    console.log("deleting string");
+    $.post("/delete_site", {
+        "url": site
+    }).done(function delete_the_site() {
+        $("tr").remove(":contains(\'" + site_url + "\')");
+        console.log("." + site_url);
+    });
+}
+
+/** Posts site to add in add_site Python function. */
+$("#add_site_button").click(function(add_site_button_clicked) {
+    $.post("/add_site", {
+        "url": $("input[name='field']").val()
+    }).done(function() {
+        console.log("new site was added");
+        $("input[name='field']").val('');
+        //$('<li>').text('New item').appendTo('.items');
+    });
+    add_site_button_clicked.preventDefault();
+});
+/** Gets result return values from site_status Python function. */
+var get_site_status = $.ajax({
+    'url': '/site_status'
+});
+get_site_status.done(function(ping_list) {
+    console.log(ping_list.result.length)
+    for (i = 0; i < ping_list.result.length; i++) {
+        $('#' + i).text(ping_list.result[i]);
+    }
+});
+get_site_status.fail(function(jqXHR, textStatus) {
+    alert('Request failed: ' + textStatus);
+});
+
+
 /**HTTP/1.1 500 Internal Server Error 
 function read_database(tables, table_columns) {
     tables = ["popular_sites", "my_sites"];
@@ -12,55 +88,11 @@ function read_database(tables, table_columns) {
         "table_columns": table_columns
     });
 }*/
-
-/** Gets site_url and icon_url return values from popular_sites_data Python function. */
-var get_popular_sites = $.ajax({
-    'url': '/popular_sites_data'
-});
-get_popular_sites.done(function(popular_sites) {
     //table_type = "popular_sites"; // code test
     //read_database(table_type); // code test
-    var popular_sites_site_url_list = popular_sites.site_url;
-    var popular_sites_icon_url_list = popular_sites.icon_url;
-    var site_urls = [];
-    var img_urls = [];
-    var table_string = "";
-    for (i = 0; i < popular_sites_icon_url_list.length; i++) {
-        site_urls.push(popular_sites_site_url_list[i][0]);
-        img_urls.push(popular_sites_icon_url_list[i][0]);
-        table_string += "<tr><td align=\"center\" width=\"64\">" +
-            "<img src=" + img_urls[i] +
-            "></td><td style=\"color: #FFFFFF\">" + site_urls[i] +
-            "</td><td>" + "Last checked 2 seconds ago" + "</td><td id=" +
-            i.toString() + " style=\"font-size:200%\"></td><td>" +
-            "Weekly Stats" + "</td></tr>";
-        //console.log(table_string)
-    }
-    document.getElementById("popular_sites").innerHTML = table_string;
-});
-/** Posts site to delete in delete_site Python function. */
-function delete_the_site(site, site_url) {
-    console.log("deleting string");
-    $.post("/delete_site", {
-        "delete_site": site
-    }).done(function delete_the_site() {
-        $("tr").remove(":contains(\'" + site_url + "\')");
-        console.log("." + site_url);
-    });
-}
-
-/** Posts site to add in add_site Python function. */
-$("#add_site_button").click(function(add_site_button_clicked) {
-    $.post("/add_site", {
-        "add_site": $("input[name='field']").val()
-    }).done(function() {
-        console.log("new site was added");
-        $("input[name='field']").val('');
-        //$('<li>').text('New item').appendTo('.items');
-    });
-    add_site_button_clicked.preventDefault();
-});
-/** Gets site_url return values from my_sites_data Python function. */
+    //var popular_sites_site_url_list = popular_sites.site_url;
+    //var popular_sites_icon_url_list = popular_sites.icon_url;
+/** Gets site_url return values from my_sites_data Python function. 
 var get_my_sites = $.ajax({
     'url': '/my_sites_data'
 });
@@ -86,17 +118,5 @@ get_my_sites.done(function(response) {
         console.log(table_string)
     }
     document.getElementById("my_sites").innerHTML = table_string;
-});
-/** Gets result return values from site_status Python function. */
-var get_site_status = $.ajax({
-    'url': '/site_status'
-});
-get_site_status.done(function(ping_list) {
-    console.log(ping_list.result.length)
-    for (i = 0; i < ping_list.result.length; i++) {
-        $('#' + i).text(ping_list.result[i]);
-    }
-});
-get_site_status.fail(function(jqXHR, textStatus) {
-    alert('Request failed: ' + textStatus);
-});
+});*/
+
