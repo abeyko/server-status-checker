e8a50eb2131c70634a22fe460b6f32f021bcee33
Angeliki Beyko <Angeliki@Angelikis-MacBook-Air.local>
2016-01-05 23:53:04 -0600

    Commit No. 60 - new structure and testing functions first time together

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import os
 import sqlite3
 import httplib
 import time
+import datetime
+import apscheduler.scheduler
+import re
+import subprocess
 
 def database_connection(decider, command):
     connection = sqlite3.connect('sqlite3.db')
@@ -33,24 +37,28 @@ class Database(object):
         table = database_connection(0, "SELECT * from sites")
 
         icon_list = []
+        db.url_list = []
         last_checked_list = []
+        status_icon_list = []
         ping_status_list = []
         ping_latency_list = []
         http_status_list = []
-        db.url_list = []
 
         for item in table:
-            db.url_list.append(item[1])
-            icon_list.append(item[2])
-            ping_latency_list.append(item[3])
-            ping_status_list.append(item[4])
-            ping_latency_list.append(item[5])
-            http_status_list.append(item[6])
+            icon_list.append(item[1])
+            db.url_list.append(item[2])
+            last_checked_list.append(item[3])
+            status_icon_list.append(item[4])
+            ping_status_list.append(item[5])
+            ping_latency_list.append(item[6])
+            http_status_list.append(item[7])
+
 
         return {
             'icon_list': icon_list,
             'url_list': db.url_list,
             'last_checked_list': last_checked_list,
+            'status_icon_list': status_icon_list,
             'ping_status_list': ping_status_list,
             'ping_latency_list': ping_latency_list,
             'http_status_list': http_status_list
@@ -66,9 +74,8 @@ class Database(object):
         """
         url = str(url)
         print 'the url that was added is'
-        command_output = database_connection(
+        database_connection(
             1, "INSERT INTO sites (site_url) VALUES (\"" + url + "\")")
-        print command_output
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
@@ -81,11 +88,15 @@ class Database(object):
         database_connection(
             1, "DELETE FROM sites WHERE Site_Url= \"" + url + "\"")
 
+
 global db
 db = Database()
+global url_list
+url_list = db.url_list
+global site
+site = Site()
 
 class Site(object):
-    url_list = db.url_list
 
     def httpredirect(self, url, depth=0):
         """Handles http redirects. Returns final http status.
@@ -137,15 +148,20 @@ class Site(object):
             print " "
             return final_status
 
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def site_status(self):
+    #@cherrypy.expose
+    def background_status_check(self):
         """Return an up or down status icon after
         checking each url in the database"""
         if len(db.url_list) == 0:
             time.sleep(1)
-        print "For checking purposes: in site_status, list is" + str(db.url_list)
-        result_list = []
+        print "For checking purposes: in background_status_check, list is" + str(db.url_list)
+        
+        status_icon_list = []
+        last_checked_list = []
+        ping_response_list = []
+        ping_latency_list = []
+        http_status_list = []
+
         for url in db.url_list:
             ping_item = url
             http_item = url
@@ -153,37 +169,61 @@ class Site(object):
             # print url
             if ping_item.startswith('http://'):
                 ping_item = ping_item[7:]
-            ping_response = os.system("ping -c 1 -W 1 " + ping_item)
+            #ping_response = os.system("ping -c 1 -W 1 " + ping_item)
+            try:
+                ping = subprocess.Popen(["ping", "-n", "-c 1", ping_item], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                out, error = ping.communicate()
+                print 'ping is ' + str(ping)
+                print 'out is ' + str(out) 
+                if out:
+                    try:
+                        ping_latency = int(re.findall(r"time=(\d+)", out)[0])
+                        ping_response = int(re.findall(r"(\d+) packets received", out)[0])
+                        print 'time is ' + str(time) + 'ms'
+                    except:
+                        print "error occurred finding time"
+                    else:
+                        print 'No ping'
+
+            except subprocess.CalledProcessError:
+                print "Couldn't get a ping"
             http_status = self.httpredirect(http_item, 0)
-            if ping_response == 0 \
+
+            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            elapsed_time = last_checked - current_time
+
+            last_checked_list.append(elapsed_time)           
+            ping_response_list.append(ping_response)
+            ping_latency_list.append(ping_latency)
+            http_status_list.append(http_status)
+
+            if ping_response == 1 \
                     or http_status >= 500 or \
                     http_status <= 399 or \
                     http_status == 405:
                 print 'http code is ' + str(http_status)
                 print 'ping response' + str(ping_response)
-                result_list.append(u"\u2705")
+                status_icon = "u2705"
+                #status_icon_list.append(u"\u2705") # ONLY PASS IN U2705 AND THEN ADD THE UNICODE BIT JS SIDE
             else:
-                result_list.append(u"\u274e")
-        return {
-            'result': result_list
-        }
+                #status_icon_list.append(u"\u274e") # OR PYTHON CAN CONVERT IT BEFORE PASSING TO JS
+                status_icon = "u274e"
+        database_connection(1, "UPDATE sites SET last_checked=elapsed_time, status_icon=status_icon, ping_status=ping_response, ping_latency=ping_latency, http_status=http_status WHERE site_url = (\"" + url + "\") ")
 
-    """@cherrypy.expose
+    @cherrypy.expose
     @cherrypy.tools.json_out()
     def single_status_check(self):
+        single = "check single status here"
 
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def background_status_check(self):
+"""def update():
+    while True:
+        site.background_status_check()
+        time.sleep(5)"""
 
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def store_last_status(self):
+sched = Scheduler()
+sched.start()
 
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def icon_upload(self):"""
-
+sched.add_job(site.background_status_check, 'interval', seconds = 300)
 
 class App(object):
 
@@ -210,6 +250,5 @@ app.site.database = Database()
 cherrypy.tree.mount(app, '/', conf)
 cherrypy.tree.mount(app.site, '/Site')
 cherrypy.tree.mount(app.site.database, '/Site/Database')
-
 cherrypy.engine.start()
 cherrypy.engine.block()
--- public/app.js
+++ public/app.js
@@ -3,16 +3,14 @@ var get_sites = $.ajax({
     'url': 'Site/Database/read_the_table'
 });
 get_sites.done(function(sites) {
-    console.log('this is sites: ');
-    console.log(sites);
-    var urls = sites.url_list;
-    console.log("urls is ");
-    console.log(urls);
-    var icons = sites.icon_list;
-    console.log("icons is ");
-    console.log(icons);
-    var site_urls = [];
-    var img_urls = [];
+    var icon_urls = sites.icon_list;
+    var site_urls = sites.url_list;
+    var last_checked = sites.last_checked_list;
+    var status_icon = sites.status_icon_list;
+    var ping_status = sites.ping_status_list;
+    var ping_latency = sites.ping_latency_list;
+    var http_status = sites.http_status_list;
+
     var table_string = "";
     var strings = ["Icon", "Site", "Last Checked", "Status Icon", "Ping Status", "Ping Latency", "HTTP Status", "Weekly Stats", "Delete Site", "Ping Site"];
     for (i = 0; i < strings.length; i++) {
@@ -21,32 +19,24 @@ get_sites.done(function(sites) {
     }
     new_table_string = "<tr>" + table_string + "</tr>";
 
-    for (i = 0; i < urls.length; i++) {
-        site_urls.push(urls[i]);
-        img_urls.push(icons[i]);
-        console.log("SITE URLS ARE");
-        console.log("IMG URLS ARE");
-        console.log(site_urls);
-        console.log(img_urls);
-        
-        
+    for (i = 0; i <site_urls.length; i++) {
         table_string += "<tr><td align=\"center\" width=\"64\">" +
-            "<img src=" + img_urls[i] +
+            "<img src=" + icon_urls[i] +
             "></td><td style=\"color: #FFFFFF\">" + site_urls[i] +
-            "</td><td>" + "Last checked 2 seconds ago" + "</td><td id=" +
-            i.toString() + " style=\"font-size:200%\"></td><td>" +
-            "Ping Status: " + "</td><td>" +
-            "Ping Latency: " + "</td><td>" +
-            "HTTP Status: " + "</td><td>" +
+            "</td><td style=\"color: #FFFFFF\">" + last_checked[i] +
+            "</td><td style=\"color: #FFFFFF\">" + status_icon[i] +
+            " style=\"font-size:200%\"></td><td style=\"color: #FFFFFF\">" + ping_status[i] +
+            "</td><td style=\"color: #FFFFFF\">" + ping_latency[i] +
+            "</td><td style=\"color: #FFFFFF\">" + http_status[i] +
+            "</td><td>" +
             "Weekly Stats" + "</td><td>" +
             "<button type=\"button\" onclick=\"delete_the_site(&#34;" +
             site_urls[i] + "&#34;, &#34;" + site_urls[i] +
             "&#34;)\">Delete</button>" + "</td><td>" +
             "Ping Now" + "</td></tr>";
-        console.log("IMG URLS [I] ARE");
-        console.log(img_urls[i]);
     }
     document.getElementById("sites").innerHTML = table_string;
+    // ever 5 min do a location.reload()
 });
 /** Posts site to delete in delete_site Python function. */
 function delete_the_site(site, site_url) {
@@ -67,22 +57,8 @@ $("#add_site_button").click(function(add_site_button_clicked) {
         console.log("new site was added");
         $("input[name='field']").val('');
         //$('#sites tr:last').after(table_row);
-        //$('<li>').text('New item').appendTo('.items');
-        // the <li> tag defines a list item
+
     });
     add_site_button_clicked.preventDefault();
-});
-/** Gets result return values from site_status Python function. */
-var get_site_status = $.ajax({
-    'url': 'Site/site_status'
-});
-get_site_status.done(function(ping_list) {
-    console.log(ping_list.result.length)
-    for (i = 0; i < ping_list.result.length; i++) {
-        $('#' + i).text(ping_list.result[i]);
-    }
-});
-get_site_status.fail(function(jqXHR, textStatus) {
-    alert('Request failed: ' + textStatus);
 });
 
